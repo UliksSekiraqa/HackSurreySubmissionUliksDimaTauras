5e45ccb148fae64be8de12c06370a7312b5e03a7
satilianius <Satilianius@gmail.com>
2019-02-10 03:01:16 +0000

    Link the pages, signup is not working yet

--- includes/signup.php
+++ includes/signup.php
@@ -1,8 +1,9 @@
 <?php
-if (!isset($_POST['submit'])){
-    header("Location: ../signup.php");
-    exit();
-};
+// if (!isset($_POST['submit'])){
+//     header("Location: ../signupForm.html");
+//     exit();
+// };
+debug_to_console($_POST);
 require_once "dbconnect.php";
 $forename = mysqli_real_escape_string($conn, $_POST["forename"]);
 $surname = mysqli_real_escape_string($conn, $_POST["surname"]);
@@ -14,6 +15,15 @@ $phone =mysqli_real_escape_string($conn, $_POST["phone"]);
 $registration_date = date("Y-m-d");
 
 $result = $conn->query
-    ("INSERT INTO users (forename, surname, email, password_hash, registration_date, gender, telephone)
-        VALUE ($forename, $surname, $email, $password, $registration_date, $gender, $phone)
+    ("INSERT INTO users (forename, surname, email, password_hash, registration_date, telephone)
+        VALUE ($forename, $surname, $email, $password, $registration_date, $phone)
     ");
+
+function debug_to_console( $data ) {
+    $output = $data;
+    if ( is_array( $output ) )
+        $output = implode( ',', $output);
+
+    echo "<script>console.log( 'Debug Objects: " . $output . "' );</script>";
+}
+?>
