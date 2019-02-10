fb198d22b537b047026cf6b2a22dfe2db655340d
satilianius <Satilianius@gmail.com>
2019-02-10 03:57:32 +0000

    fix signup php, it stores data in db now

--- includes/dbconnect.php
+++ includes/dbconnect.php
@@ -6,7 +6,9 @@ $dbName = "MakeItDone";
 
 $conn = mysqli_connect($dbServerName, $dbUsername, $dbPassword, $dbName);
 if ($conn->connect_error) {
+    echo "<script>console.log( 'connection failed' );</script>";
     die("Connection failed: " . $conn->connect_error);
 }
+echo "<script>console.log( 'connection established' );</script>";
 
 ?>
--- includes/signup.php
+++ includes/signup.php
@@ -8,15 +8,17 @@ require_once "dbconnect.php";
 $forename = mysqli_real_escape_string($conn, $_POST["forename"]);
 $surname = mysqli_real_escape_string($conn, $_POST["surname"]);
 $email = mysqli_real_escape_string($conn, $_POST["email"]);
-$password = mysqli_real_escape_string($conn, $_POST["password"]);
+$password = mysqli_real_escape_string($conn, $_POST["pass"]);
 $confirm_password = mysqli_real_escape_string($conn, $_POST["confpass"]);
 $gender = mysqli_real_escape_string($conn, $_POST["gender"]);
 $phone =mysqli_real_escape_string($conn, $_POST["phone"]);
 $registration_date = date("Y-m-d");
 
-$result = $conn->query
-    ("INSERT INTO users (forename, surname, email, password_hash, registration_date, telephone)
-        VALUE ($forename, $surname, $email, $password, $registration_date, $phone)
+$pass_hash = password_hash($password, PASSWORD_DEFAULT);
+
+$conn->query
+    ("INSERT INTO users (forename, surname, email, password_hash, registration_date, telephone, gender)
+        VALUES ('$forename', '$surname', '$email', '$pass_hash', '$registration_date', '$phone', '$gender')
     ");
 
 function debug_to_console( $data ) {
